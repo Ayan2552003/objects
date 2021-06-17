3a11d38b284b1a1ad28b2cd70eecb37542d04fae
Syed Ayan Adeel <ayanadeelpak@gmail.com>
2021-06-17 20:22:52 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,47 @@
+// q1
+// var itemsArray = [
+//     {name:"juice",price:50, quantity:3},
+//     {name:'cookie',price:30, quantity:9},
+//     {name:'shirt',price:880, quantity:1},
+//     {name:'pen',price:100, quantity:2}];
+//     let total = [];
+//     let totalPrice = 0;
+//     for (let i = 0; i < itemsArray.length; i++) {
+//         total.push(itemsArray[i].price * itemsArray[i].quantity);
+//         totalPrice = totalPrice + total[i];
+
+//     }
+
+    // q2
+//     var obj = {
+//         name: "Ayan",
+//         age: 18,
+//         gender: "male",
+//         email: "ayanadeel@gmail.com",
+//         city: "Karachi",
+//         country: "Pakistan"
+//     };
+//     console.log("name" in obj);
+//     console.log("age" in obj);
+//     console.log("firstName" in obj);
+//     console.log("lastName" in obj);
+//     console.log("city" in obj);
+//     console.log(obj.city);
+
+    // q3
+//     function Constructor(name, age) {
+//         this.name = name;a
+//         this.age = age;
+//     }
+
+//     var obj1 = new Constructor('Ayan',18);
+//     var obj2 = new Constructor('Ahmed', 19);
+
+    // q4
+//     function Population(name,gender,address,education, profession) {
+//         this.name = name;
+//         this.gender = gender;
+//         this.address = address;
+//         this.education = education;
+//         this.profession = profession;
+//     }
